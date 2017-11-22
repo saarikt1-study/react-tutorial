92723ea85c22695677cbbffb49954bb1656c5c17
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2017-11-22 15:22:19 +0200

    Add youtube search function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTsearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
-const API_KEY = process.env.API_KEY
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss'//process.env.API_KEY
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTsearch({key: API_KEY, term: 'cats'}, (videos) => {
+      this.setState({ videos })
+    })
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    ) 
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
